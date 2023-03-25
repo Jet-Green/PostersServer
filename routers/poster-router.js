e03e06934d1826d09c5eef409d06528d0f953722
regtyit <titov54275@gmail.com>
2023-03-25 12:44:25 +0400

    small fix

--- routers/poster-router.js
+++ routers/poster-router.js
@@ -7,8 +7,8 @@ const PosterController = require('../controllers/poster-controller')
 const router = Router()
 
 //routes
-router.get('/create', PosterController.create)
+router.post('/create', PosterController.create)
 router.post('/get-all', PosterController.getAll)
 router.get('/find-by-id', PosterController.findById)
-router.get('/delete-one', PosterController.deleteById)
+router.post('/delete-one', PosterController.deleteById)
 router.get('/clear', PosterController.deleteMany)
