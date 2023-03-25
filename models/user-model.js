53e99e75f3f7d075290c6bbb6ce4616473fe1222
JetGreen <grishadzyin@gmail.com>
2023-03-25 11:58:26 +0400

    typos

--- models/user-model.js
+++ models/user-model.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-    email: { type: String},
-    password: { type: String},
-    firstname: { type: String},
-    lastname: { type: String},
-    phone: { type: String},
+    email: { type: String },
+    password: { type: String },
+    firstname: { type: String },
+    lastname: { type: String },
+    phone: { type: String },
     roles: { type: Array },
     posters: { type: Array },
     subscribtion: { type: Object },
