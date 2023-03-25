0570c76c458821944aa3246456f12f438a806035
JetGreen <grishadzyin@gmail.com>
2023-03-25 19:58:32 +0400

    update usr dto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -1,7 +1,7 @@
 class UserDto {
-    email; firstname; lastname; phone; roles; posters; subscription;
+    email; firstname; lastname; phone; roles; posters; subscription; _id;
     constructor(user) {
-        const { email, firstname, lastname, phone, roles, posters, subscription } = user
+        const { email, firstname, lastname, phone, roles, posters, subscription, _id } = user
         this.email = email;
         this.firstname = firstname;
         this.lastname = lastname;
@@ -9,6 +9,7 @@ class UserDto {
         this.roles = roles;
         this.posters = posters;
         this.subscription = subscription;
+        this._id = _id
     }
 }
 
