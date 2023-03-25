b4485db8c6a5c3ba1988e0ad4caa4b581bcd8280
regtyit <titov54275@gmail.com>
2023-03-25 11:39:02 +0400

    Models

---
+++ models/user-model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    email: { type: String},
+    password: { type: String},
+    firstname: { type: String},
+    lastname: { type: String},
+    phone: { type: String},
+    roles: { type: Array },
+    posters: { type: Array },
+    subscribtion: { type: Object },
+})
+
+module.exports = model('User', UserSchema);
