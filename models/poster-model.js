4445f890cef3584592118b3f98e91114f84a05d4
JetGreen <grishadzyin@gmail.com>
2023-03-26 04:11:05 +0400

    update model

--- models/poster-model.js
+++ models/poster-model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const PosterSchema = new Schema({
     image: { type: String },
     title: { type: String },
-    eventLocation: { type: String },
+    eventLocation: { type: Object },
     site: { type: String },
     organizer: { type: String },
     phone: { type: String },
