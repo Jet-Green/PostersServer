98f7153d0ba8a9711796432dd79b9dc3e439f029
Егор Князев <egorknyazev10@gmail.com>
2023-06-12 17:23:49 +0400

    added api error

---
+++ exception/api-error.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+	status
+	errors
+
+	constructor(status, message, errors = []) {
+		super(message);
+		this.status = status;
+		this.errors = errors;
+	}
+
+	static UnauthorizedError() {
+		return new ApiError(401, 'Вы не авторизованы')
+	}
+
+	static AccessDenied() {
+		return new ApiError(403, 'Отказано в доступе')
+	}
+
+	static BadRequest(message, errors = []) {
+		return new ApiError(400, message, errors)
+	}
+}
+
+module.exports = ApiError
