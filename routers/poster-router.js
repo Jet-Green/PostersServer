006f3d6786e02fe68f0b10ac4431f432a5da99cc
JetGreen <grishadzyin@gmail.com>
2023-07-04 15:59:34 +0400

    moderation

--- routers/poster-router.js
+++ routers/poster-router.js
@@ -16,7 +16,7 @@ router.get('/delete-by-id', PosterController.deleteById)
 router.get('/get-posters-on-moderaion', PosterController.getPostersOnModeration)
 
 router.get('/get-by-id', PosterController.getById)
-router.get('/get-posters-to-moderation' ,PosterController.getPostersToModeration)
+router.get('/get-posters-to-moderation', PosterController.getPostersOnModeration)
 router.post('/get-user-posters', PosterController.getUserPosters)
 router.post('/update', PosterController.updatePoster)
 router.get('/clear', PosterController.deleteMany)
