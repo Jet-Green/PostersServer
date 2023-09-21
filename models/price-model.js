881a7c12d18b1115626ea5a56ff431f174971735
qbclub <17926057+qbclub@users.noreply.github.com>
2023-09-21 22:03:53 +0400

    fix

--- models/price-model.js
+++ models/price-model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const PriceSchema = new Schema({
     name: { type: String },
     price: { type: Number },
-    salePrice: { type: Number },
+    profit: { type: Number },
     numberPosters: { type: Number },
 })
 
