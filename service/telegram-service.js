0e58ac4136296553d6d1dc1d8776cb26df0a1ef5
JetGreen <grishadzyin@gmail.com>
2023-11-01 10:04:54 +0400

    disable tg bot on development

--- service/telegram-service.js
+++ service/telegram-service.js
@@ -3,21 +3,24 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_API_TOKEN
 const chat_id = process.env.TELEGRAM_CHAT_ID
 
-const bot = new TelegramBot(token, { polling: true })
+if (process.env.NODE_ENV == 'production') {
+    const bot = new TelegramBot(token, { polling: true })
+}
 
-module.exports = {
+// экспортирую объект с пустыми функциями, если если мод development
+module.exports = process.env.NODE_ENV == 'production' ? {
     async sendPost(poster) {
         try {
             console.log(process.env.CLIENT_URL + '/post?_id=' + poster._id)
             await bot.sendPhoto(
-                chat_id, 
-                poster.image, 
-                { 
-                    caption: `<a href="${process.env.CLIENT_URL + '/post?_id=' + poster._id}">${poster.eventType.join(' | ')}   "${poster.title}" </a>`, parse_mode: 'HTML' 
+                chat_id,
+                poster.image,
+                {
+                    caption: `<a href="${process.env.CLIENT_URL + '/post?_id=' + poster._id}">${poster.eventType.join(' | ')}   "${poster.title}" </a>`, parse_mode: 'HTML'
                 }
             )
-        } catch(error) {
+        } catch (error) {
             console.error(error)
         }
     }
-}
+} : { async sendPost(poster) { return } }
