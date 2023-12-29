451740985b9ccc25728da05d5797d122cbe35672
JetGreen <grishadzyin@gmail.com>
2023-12-29 16:58:02 +0400

    fix urls

--- controllers/poster-controller.js
+++ controllers/poster-controller.js
@@ -12,10 +12,10 @@ module.exports = {
     },
     async moderatePoster(req, res, next) {
         try {
-            await vkapi.postInGroup(
-                `${process.env.CLIENT_URL}/post?_id=${req.query._id}`,
+            if (process.env.NODE_ENV == 'production') {
+                vkapi.postInGroup(`${process.env.CLIENT_URL}/post?_id=${req.query._id}`)
+            }
 
-            )
             return res.json(await PosterService.moderatePoster(req.query._id, req.query.userId))
         } catch (error) {
             next(error)
@@ -57,8 +57,6 @@ module.exports = {
                     ${JSON.stringify(req.body)}
                     </body>
                     </html>`, emails = ['grachevrv@ya.ru', 'grishadzyin@gmail.com'], 'Создана афиша')
-
-                vkapi.postInGroup(`https://plpo.ru/post?_id=${posterId}`)
             }
 
             return res.json({ _id: posterId, message: 'Создано' })
