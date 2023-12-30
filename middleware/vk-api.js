6041a48f504e3921362f748cf76fc30aefcb0aa8
qbclub <17926057+qbclub@users.noreply.github.com>
2023-12-30 09:28:59 +0400

    ашч

--- middleware/vk-api.js
+++ middleware/vk-api.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
 
 module.exports = process.env.NODE_ENV == 'production' ? {
-    async postInGroup(link, eventType) {
-        let response = await axios.post(`https://api.vk.com/method/wall.post?access_token=${process.env.VK_ACCESS_TOKEN}&v=5.131&attachments=${link}&message=${eventType.join('|')} ${message}&owner_id=-222755810&from_group=1`)
+    async postInGroup(message) {
+        let response = await axios.post(`https://api.vk.com/method/wall.post?access_token=${process.env.VK_ACCESS_TOKEN}&v=5.131&attachments=${message}&message=${message}&owner_id=-222755810&from_group=1`)
         return response
     }
-} : { async postInGroup(link) { } }
+} : { async postInGroup(message) { } }
 
 // `https://oauth.vk.com/authorize?client_id=-51783056&scope=manage&redirect_uri=http://localhost:3031&response_type=token`
 // https://oauth.vk.com/access_token?client_id=51783056&client_secret=Hn2RIxtq3uzc3Vl0KAgI&redirect_uri=http://localhost:3031&code=0a6421a65a4a8219e6
